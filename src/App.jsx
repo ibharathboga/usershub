e9bb36d7951eee2dddf1235b08f484d01f60cdb8
ibharathboga <ibharathboga@gmail.com>
2025-09-21 22:36:14 +0530

    install axios and showcase a demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const axiosDemo = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts/1"
+    try {
+      const response = await axios.get(url);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => { axiosDemo(); }, []);
 
   return (
     <div className='border-2 border-red-500 flex flex-col max-w-[500px] mx-auto'>
