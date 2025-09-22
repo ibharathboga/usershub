de5bec44f18bb4ebf2183996217ccc7e4112dcf7
ibharathboga <ibharathboga@gmail.com>
2025-09-22 10:38:12 +0530

    showcase useUsers hook demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,50 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
+import "./App.css";
+import useUsers from "./hooks/useUsers";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const { users, loading, addUser, editUser, deleteUser } = useUsers();
 
-  const axiosDemo = async () => {
-    const url = "https://jsonplaceholder.typicode.com/posts/1"
-    try {
-      const response = await axios.get(url);
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  useEffect(() => { axiosDemo(); }, []);
+  const baseUser = {
+    name: "john snow",
+    email: "johnsnow@gmail.com",
+    company: { name: "gameofthrones" },
+  };
 
   return (
-    <div className='border-2 border-red-500 flex flex-col max-w-[500px] mx-auto'>
-      <p className='text-center'>{count}</p>
-      <button onClick={() => setCount(prev => prev + 1)}>increment</button>
-      <button onClick={() => setCount(prev => prev - 1)}>decrement</button>
-    </div>
-  )
-}
+    <div className="p-4">
+      <p className="mb-4">loading: {String(loading)}</p>
 
-export default App
+      <div className="flex flex-col gap-2">
+        <button
+          className="border px-2 py-1"
+          onClick={() =>
+            addUser({ ...baseUser, name: "random-user@" + Date.now(), id: String(Date.now()) })
+          }
+        >
+          ➕ Add User
+        </button>
+
+        <button
+          onClick={() =>
+            editUser({ userId: 1758516065792, showSuccess: true, updatedFields: { email: "snowjohn@gmail.com", name: "edited-name@" + Date.now() } })
+          }
+          className="border px-2 py-1"
+        >
+          Edit User (1758516065792)
+        </button>
+
+
+        <button
+          className="border px-2 py-1"
+          onClick={() => deleteUser(1758516065792)}
+        >
+          🗑️ Delete User (id {1758516065792})
+        </button>
+      </div>
+      <h2 className="font-bold">Users</h2>
+      <pre className="border p-2 bg-gray-100 mb-4">
+        {JSON.stringify(users, null, 2)}
+      </pre>
+    </div >
+  );
+}
