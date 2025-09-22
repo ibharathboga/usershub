651705572404890a4200caad8c1e197ae4850667
ibharathboga <ibharathboga@gmail.com>
2025-09-22 10:37:50 +0530

    setup axios instance and showcase demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import iaxios from './utils/axios';
 
 function App() {
   const [count, setCount] = useState(0)
 
   const axiosDemo = async () => {
-    const url = "https://jsonplaceholder.typicode.com/posts/1"
     try {
-      const response = await axios.get(url);
+      const response = await iaxios.get("/");
       console.log(response);
     } catch (error) {
       console.log(error);
     }
   }
-
   useEffect(() => { axiosDemo(); }, []);
 
   return (
     <div className='border-2 border-red-500 flex flex-col max-w-[500px] mx-auto'>
       <p className='text-center'>{count}</p>
-      <button onClick={() => setCount(prev => prev + 1)}>increment</button>
+      <button onClick={() => setCount(prev => prev + 1)}>incrasdfasdfement</button>
       <button onClick={() => setCount(prev => prev - 1)}>decrement</button>
     </div>
   )
---
+++ src/utils/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const iaxios = axios.create({
+  baseURL: "https://68d0da6ce6c0cbeb39a2a3fa.mockapi.io/userhub/users",
+
+
+});
+
+export default iaxios;
