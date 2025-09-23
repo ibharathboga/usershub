64b13f8b2a76dfb49da28b9da22fe5a888d35f19
ibharathboga <ibharathboga@gmail.com>
2025-09-24 03:45:06 +0530

    useUserForm controls

---
+++ src/hooks/useUserForm.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function useUserForm() {
+  const [formVisible, setFormVisible] = useState(false);
+  const [editingUser, setEditingUser] = useState(null);
+
+  const handleAddClick = () => {
+    setEditingUser(null);
+    setFormVisible(true);
+  };
+
+  const handleEditClick = (user) => {
+    console.log(user.id);
+    setEditingUser(user);
+    setFormVisible(true);
+  };
+
+  const onCancel = () => {
+    setFormVisible(false);
+    setEditingUser(null);
+  }
+
+  return {
+    setFormVisible,
+    setEditingUser,
+    handleAddClick,
+    handleEditClick,
+    formVisible,
+    editingUser, onCancel
+  };
+}
+
+export default useUserForm;
