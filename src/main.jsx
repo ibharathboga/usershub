ecbdea93fccb4839787c2f84a137bb6ee449f229
ibharathboga <ibharathboga@gmail.com>
2025-09-23 16:26:30 +0530

    wrap App with UsersProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+import { UsersProvider } from './UsersProvider.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <UsersProvider>
+      <App />
+    </UsersProvider >
   </StrictMode>,
 )
