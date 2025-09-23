9cf2fb53595bbf4116493d427cb2133029f92753
ibharathboga <ibharathboga@gmail.com>
2025-09-23 16:48:11 +0530

    wrap correction, main.jsx, UsersProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { UsersProvider } from './UsersProvider.js'
+import { UsersProvider } from './UsersProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
